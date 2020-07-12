269f10e2b62e73891ec644340a533e27895ef4ed
corlys <duscae047@gmail.com>
2020-07-12 12:01:10 +0700

    C:/Program Files/Git/home

--- webtimer/updater.py
+++ webtimer/updater.py
@@ -5,5 +5,5 @@ from webtimer import history_job
 
 def start():
 	scheduler = BackgroundScheduler()
-	scheduler.add_job(history_job.update_history, 'interval', minutes=60)
+	scheduler.add_job(history_job.update_history, 'interval', minutes=10)
 	scheduler.start()
