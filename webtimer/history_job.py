36dd83546eed213ff58622967e20dac8e5a322e1
corlys <duscae047@gmail.com>
2020-07-19 11:21:55 +0700

    tambah threading

--- webtimer/history_job.py
+++ webtimer/history_job.py
@@ -1,13 +1,17 @@
 from urllib.request import urlopen
 from time import time
+import threading
 
 from .models import History, Webtimer
 
 def update_history():
 	try:
 		tableone = Webtimer.objects.all()
-		loadtime_counter(tableone);
-		print('masuk update history')
+		start_time = time()
+		loadtime_counter(tableone)
+		# start_thread(tableone)
+		end_time = time()
+		print(round(end_time-start_time, 3))
 	except Exception as e:
 		print(str(e))
 
@@ -26,3 +30,29 @@ def loadtime_counter(links):
 			link.save()
 			new_history = History(webtimer = link, loadtime = link.time)
 			new_history.save()
+
+# def loadtime_counter(link):
+# 	try:
+# 		start_time = time()
+# 		stream = urlopen(link.urls)
+# 		output = stream.read()
+# 		end_time = time()
+# 		stream.close()
+# 		elapsed_time = round(end_time-start_time, 3)
+# 	finally:
+# 		link.time = elapsed_time
+# 		link.save()
+# 		new_history = History(webtimer = link, loadtime = link.time)
+# 		new_history.save()
+
+# def start_thread(links):
+# 	threads = []
+# 	for link in links:
+# 		t = threading.Thread(target=loadtime_counter, args=(link,))
+# 		threads.append(t)
+
+# 	for t in threads:
+# 		t.start()
+
+# 	for t in threads:
+# 		t.join()
