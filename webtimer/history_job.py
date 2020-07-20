cfbea0334da8fb99b19c962f7aa3d184b125b2af
corlys <duscae047@gmail.com>
2020-07-20 10:21:47 +0700

    fixfix

--- webtimer/history_job.py
+++ webtimer/history_job.py
@@ -8,51 +8,51 @@ def update_history():
 	try:
 		tableone = Webtimer.objects.all()
 		start_time = time()
-		loadtime_counter(tableone)
-		# start_thread(tableone)
+		# loadtime_counter(tableone)
+		start_thread(tableone)
 		end_time = time()
-		print(round(end_time-start_time, 3))
+		# print(round(end_time-start_time, 3))
 	except Exception as e:
 		print(str(e))
 
 
-def loadtime_counter(links):
-	for link in links:
-		try:
-			start_time = time()
-			stream = urlopen(link.urls)
-			output = stream.read()
-			end_time = time()
-			stream.close()
-			elapsed_time = round(end_time-start_time, 3)
-		finally:
-			link.time = elapsed_time
-			link.save()
-			new_history = History(webtimer = link, loadtime = link.time)
-			new_history.save()
-
-# def loadtime_counter(link):
-# 	try:
-# 		start_time = time()
-# 		stream = urlopen(link.urls)
-# 		output = stream.read()
-# 		end_time = time()
-# 		stream.close()
-# 		elapsed_time = round(end_time-start_time, 3)
-# 	finally:
-# 		link.time = elapsed_time
-# 		link.save()
-# 		new_history = History(webtimer = link, loadtime = link.time)
-# 		new_history.save()
-
-# def start_thread(links):
-# 	threads = []
+# def loadtime_counter(links):
 # 	for link in links:
-# 		t = threading.Thread(target=loadtime_counter, args=(link,))
-# 		threads.append(t)
+# 		try:
+# 			start_time = time()
+# 			stream = urlopen(link.urls)
+# 			output = stream.read()
+# 			end_time = time()
+# 			stream.close()
+# 			elapsed_time = round(end_time-start_time, 3)
+# 		finally:
+# 			link.time = elapsed_time
+# 			link.save()
+# 			new_history = History(webtimer = link, loadtime = link.time)
+# 			new_history.save()
 
-# 	for t in threads:
-# 		t.start()
+def loadtime_counter(link):
+	try:
+		start_time = time()
+		stream = urlopen(link.urls)
+		output = stream.read()
+		end_time = time()
+		stream.close()
+		elapsed_time = round(end_time-start_time, 3)
+	finally:
+		link.time = elapsed_time
+		link.save()
+		new_history = History(webtimer = link, loadtime = link.time)
+		new_history.save()
 
-# 	for t in threads:
-# 		t.join()
+def start_thread(links):
+	threads = []
+	for link in links:
+		t = threading.Thread(target=loadtime_counter, args=(link,))
+		threads.append(t)
+
+	for t in threads:
+		t.start()
+
+	for t in threads:
+		t.join()
--- webtimer/updater.py
+++ webtimer/updater.py
@@ -5,5 +5,5 @@ from webtimer import history_job
 
 def start():
 	scheduler = BackgroundScheduler()
-	scheduler.add_job(history_job.update_history, 'interval', minutes=2)
+	scheduler.add_job(history_job.update_history, 'interval', hours=1)
 	scheduler.start()
